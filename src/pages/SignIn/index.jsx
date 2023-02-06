05ae2caa1ca099688e168dce32b0219fb7d7d826
Christophe Simon <csimon.developpement@gmail.com>
2023-02-06 16:09:46 +0000

    Add sign in management and redirection to user page

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,26 +1,68 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 import '../../styles/SignIn.css'
 
 function SignIn() {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [error, setError] = useState('')
+  const navigate = useNavigate()
+
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+    try {
+      const response = await fetch('http://localhost:3001/api/v1/user/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          username,
+          password,
+        }),
+      })
+      const data = await response.json()
+      if (data.error) {
+        setError(data.error)
+        return
+      }
+      localStorage.setItem('token', data.token)
+      navigate('/user')
+    } catch (error) {
+      setError('An error occured, please try again later')
+    }
+  }
+
   return (
-    <main class="main bg-dark">
-      <section class="sign-in-content">
-        <i class="fa fa-user-circle sign-in-icon"></i>
+    <main className="main bg-dark">
+      <section className="sign-in-content">
+        <i className="fa fa-user-circle sign-in-icon" />
         <h1>Sign In</h1>
-        <form>
-          <div class="input-wrapper">
-            <label for="username">Username</label
-            ><input type="text" id="username" />
+        {error && <p className="error">{error}</p>}
+        <form onSubmit={handleSubmit}>
+          <div className="input-wrapper">
+            <label htmlFor="username">Username</label>
+            <input
+              type="text"
+              id="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
           </div>
-          <div class="input-wrapper">
-            <label for="password">Password</label
-            ><input type="password" id="password" />
+          <div className="input-wrapper">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
           </div>
-          <div class="input-remember">
+          <div className="input-remember">
             <input type="checkbox" id="remember-me" />
-            <label for="remember-me">Remember me</label>
+            <label htmlFor="remember-me">Remember me</label>
           </div>
-          <button class="sign-in-button">Sign In</button>
+          <button type="submit" className="sign-in-button">Sign In</button>
         </form>
       </section>
     </main>
